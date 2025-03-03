9dc99979a306d7515966cc165ad30fec53f156b6
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-03-03 01:28:18 -0300

    options to search different categories

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.blog_view, name = 'blog'),
+    path('category/<int:id_category>',views.blog_category, name='blog_categories')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ from .models import Category,Post
 
 # Create your views here.
 def blog_view(request):
-    categories_all = Category.objects.all()
+    categories_all = Category.objects.all().distinct()
     post_all = Post.objects.all()
     return render(request, 'blog.html',{'categories_all' : categories_all, 'post_all' : post_all})
+
+def blog_category(request, id_category):
+    category_filter = Category.objects.get(id=id_category)
+    post_filter = Post.objects.filter(category = category_filter)
+    categories__all = Category.objects.all().distinct()
+    return render(request, 'category_filter.html',{"categories_all" : category_filter, "post_all": post_filter, "categories__all" : categories__all})
