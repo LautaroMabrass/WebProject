c6d05ea37c69e04af47fb39b5b20095df9d32a2b
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-03-05 18:26:49 -0300

    adding the option to send emails in the contact interface

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render,redirect
 from .forms import newForm
+from django.core.mail import EmailMessage
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from .forms import newForm
+from django.core.mail import EmailMessage
+
 def contact_view(request):
     if request.method == 'GET':
-        return render(request, 'contact.html', {'new_form' : newForm()})
+        return render(request, 'contact.html', {'new_form': newForm()})
     else:
         form_ = newForm(data=request.POST)
         if form_.is_valid():
             name = request.POST['name']
-            email = request.POST['email']
+            client_email = request.POST['email']
             content = request.POST['content']
 
-        print(f"{name},{email},{content}")
-        return redirect('/contact/?valid')
+            email = EmailMessage(
+                f'Message from Client {name}',
+                f'A client with the email address {client_email} sent the following message: \n {content}',
+                'Contacto Web <appwebmail2025@gmail.com>',
+                ['lauti.mabras@gmail.com'],
+                reply_to=[client_email],
+            )
+
+            try:
+                email.send()
+                return redirect('/contact/?valid')
+            except:
+                return redirect('/contact/?notvalid')
