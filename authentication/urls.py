70ea734c4c4b6da53242f7a44cab6b31f2de1d9a
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-03-06 23:55:23 -0300

    Intefrace login, logout and register complete

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import Vregister
+from .views import view_close, close_sesion, login_view
 urlpatterns = [
     path('',Vregister.as_view(), name = 'auth'),
+    path('logout/',view_close, name = 'logout'),
+    path('end/', close_sesion, name = 'end'),
+    path('login/', login_view, name = 'login_')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
 
 class Vregister(View):
@@ -9,4 +11,40 @@ class Vregister(View):
         form = UserCreationForm()
         return render(request, 'auth.html', {'form': form})
     def post(self,request):
-        pass
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+
+            user = form.save() # alamcena automaticamente en auth usuario y pass encriptada
+
+            login(request, user) # log automatico
+
+            return redirect('home')
+        else:
+            return render(request, 'auth.html', {'form': form})
+
+def close_sesion(request):
+    logout(request)
+    return redirect('home')
+
+def view_close(request):
+    return render(request, 'closeSesion.html')
+
+def login_view(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data.get('username')
+            password_name = form.cleaned_data.get('password')
+            user_pass = authenticate(username=user_name,password = password_name)
+            if user_pass is not None:
+                login(request, user_pass)
+                return redirect('home')
+            else:
+                messages.error(request, 'User invalid')
+        else:
+            messages.error(request, 'Something goes wrong')
+            
+    form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
